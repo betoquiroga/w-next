b77793efdb4473af9e211354269b262361f569ff
Beto Jr <beto.m12@gmail.com>
2023-02-11 20:21:28 -0400

    Refactoriza estructura de carpetas para iconos

--- public/components/Navigation/MainMenu.tsx
+++ public/components/Navigation/MainMenu.tsx
@@ -1,22 +1,20 @@
-import {
-  Bible,
-  Config,
-  Menu,
-  Photo,
-  Profile,
-  Song,
-} from "../../common/Icons/index"
+import BibleIcon from "public/common/Icons/misc/bible"
+import ConfigIcon from "public/common/Icons/misc/config"
+import MenuIcon from "public/common/Icons/misc/menu"
+import PhotoIcon from "public/common/Icons/misc/photo"
+import ProfileIcon from "public/common/Icons/misc/profile"
+import SongIcon from "public/common/Icons/misc/song"
 import MainMenuItem from "./MainMenuItem"
 
 const MainMenu = () => {
   return (
     <nav className="bg-ww-content text-ww-normal w-[4rem] fixed h-screen">
-      <MainMenuItem icon={Menu} />
-      <MainMenuItem active icon={Song} />
-      <MainMenuItem icon={Bible} />
-      <MainMenuItem icon={Photo} />
-      <MainMenuItem icon={Config} />
-      <MainMenuItem icon={Profile} />
+      <MainMenuItem icon={MenuIcon} />
+      <MainMenuItem active icon={SongIcon} />
+      <MainMenuItem icon={BibleIcon} />
+      <MainMenuItem icon={PhotoIcon} />
+      <MainMenuItem icon={ConfigIcon} />
+      <MainMenuItem icon={ProfileIcon} />
     </nav>
   )
 }
--- public/modules/Songs/sections/LyricsPanels/LyricsOptions.tsx
+++ public/modules/Songs/sections/LyricsPanels/LyricsOptions.tsx
@@ -1,21 +1,19 @@
-import {
-  Bible,
-  Config,
-  Photo,
-  Profile,
-  Song,
-} from "../../../../common/Icons/index"
+import BibleIcon from "public/common/Icons/misc/bible"
+import ConfigIcon from "public/common/Icons/misc/config"
+import PhotoIcon from "public/common/Icons/misc/photo"
+import ProfileIcon from "public/common/Icons/misc/profile"
+import SongIcon from "public/common/Icons/misc/song"
 
 const LyricsOptions = () => {
   return (
     <div className="pb-4 flex justify-between w-full">
       <span>Live edit</span>
       <div className="grid-cols-5 grid gap-4 self-center">
-        <Song />
-        <Bible />
-        <Photo />
-        <Config />
-        <Profile />
+        <SongIcon />
+        <BibleIcon />
+        <PhotoIcon />
+        <ConfigIcon />
+        <ProfileIcon />
       </div>
     </div>
   )
