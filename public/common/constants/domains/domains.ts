619c001a9754e33e18da6544cb552d9406d693dc
Beto Jr <beto.m12@gmail.com>
2023-02-11 21:17:40 -0400

    cambia variables publicas y quita .env del repo

--- public/common/constants/domains/domains.ts
+++ public/common/constants/domains/domains.ts
@@ -1,2 +1,2 @@
-export const WW_API_DOMAIN = process.env.API_URL || "localhost:3040"
-export const WW_API_JSON = process.env.API_URL || "localhost:3020"
+export const WW_API_DOMAIN = process.env.NEXT_PUBLIC_API_URL || "localhost:3040"
+export const WW_API_JSON = process.env.NEXT_PUBLIC_API_URL || "localhost:3020"
