fea9264ac292c45398dd321a248dbcdea40b2285
Beto Jr <beto.m12@gmail.com>
2023-03-18 08:31:02 -0400

    Cambia dominio de subida de imagenes

--- src/modules/Gallery/sections/FilesSection.tsx
+++ src/modules/Gallery/sections/FilesSection.tsx
@@ -1,5 +1,6 @@
 import TabsHeader from "@components/Tabs/TabsHeader"
 import { Tab } from "@headlessui/react"
+import { WW_API_DOMAIN } from "src/common/constants/domains"
 import ImagesPanel from "./ImagesSection/ImagesPanel"
 import UploadPanel from "./ImagesSection/UploadPanel"
 import VideosPanel from "./ImagesSection/VideosPanel"
@@ -12,7 +13,7 @@ const FilesSection = () => {
         <Tab.Panels className="p-4">
           <ImagesPanel />
           <VideosPanel />
-          <UploadPanel endpoint={"http://localhost:3040/uploads"} />
+          <UploadPanel endpoint={`http://${WW_API_DOMAIN}/uploads`} />
         </Tab.Panels>
       </Tab.Group>
     </div>
