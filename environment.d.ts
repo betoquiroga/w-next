20e0e7bf7434a0ac1d5f1fb059572899ed66dc74
Beto Jr <beto.m12@gmail.com>
2023-03-26 07:46:52 -0400

    config environments

--- environment.d.ts
+++ environment.d.ts
@@ -1,9 +1,9 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      WS_HOSTNAME?: string;
-      WS_PORT?: string;
-      NEXT_PUBLIC_WS_HOST: string;
+      WS_HOSTNAME?: string
+      WS_PORT?: string
+      NEXT_PUBLIC_WS_HOST: string
     }
   }
 }
