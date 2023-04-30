e5e8df2fbc9a7e66075478a7bd89c9f8a20fb173
Beto Jr <beto.m12@gmail.com>
2023-04-30 10:08:04 -0400

    arrgela imagenes y daña estilos

--- src/common/helpers/images.helper.ts
+++ src/common/helpers/images.helper.ts
@@ -1,9 +1,9 @@
 import { WW_API_DOMAIN } from "../constants/domains"
 
 export const bigImage = (image: string): string => {
-  return `http://${WW_API_DOMAIN}/uploads/big/${image}`
+  return `http://${WW_API_DOMAIN}/uploads/styles/big/${image}`
 }
 
 export const smallImage = (image: string): string => {
-  return `http://${WW_API_DOMAIN}/uploads/small/${image}`
+  return `http://${WW_API_DOMAIN}/uploads/styles/small/${image}`
 }
