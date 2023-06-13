5c915ec1597a17b755a4085161b6872893b53e71
Beto Quiroga <beto.m12@gmail.com>
2023-06-13 07:09:34 -0400

    Agrega navegacion por teclas en la biblia

--- src/modules/Bible/BibleView.tsx
+++ src/modules/Bible/BibleView.tsx
@@ -1,11 +1,27 @@
+import { ChapterContext } from "@context/ChapterContext"
+import { useContext } from "react"
 import BooksSection from "./sections/BooksSection"
 import ChaptersSection from "./sections/ChaptersSection"
 import PreviewSection from "./sections/PreviewSection"
 import VersesSection from "./sections/VersesSection"
 
 const BibleView = () => {
+  const { setNextVerse, setPrevVerse } = useContext(ChapterContext)
+
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    if (event.key === "k") {
+      setNextVerse()
+    }
+    if (event.key === "i") {
+      setPrevVerse()
+    }
+  }
+
   return (
-    <div className="grid grid-cols-12 gap-4 p-4 w-[calc(100%-2px)] justify-items-stretch pl-[5rem] h-[calc(100vh-1px)] grid-rows-none">
+    <div
+      onKeyDown={handleKeyPress}
+      className="grid grid-cols-12 gap-4 p-4 w-[calc(100%-2px)] justify-items-stretch pl-[5rem] h-[calc(100vh-1px)] grid-rows-none"
+    >
       <BooksSection />
       <ChaptersSection />
       <VersesSection />
