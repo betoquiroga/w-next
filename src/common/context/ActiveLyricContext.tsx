54c11136ad6f706715a39feac750e8f27c1a8984
Beto Quiroga <beto.m12@gmail.com>
2023-06-13 07:57:42 -0400

    Refactoriza funciones en el contexto de la letra activa

--- src/common/context/ActiveLyricContext.tsx
+++ src/common/context/ActiveLyricContext.tsx
@@ -15,17 +15,16 @@ const ActiveLyricProvider = ({ children }: ActiveLyricProviderProps) => {
   const [activeLyricId, setActiveLyricId] = useState(0)
   const { data } = useContext(SongContext)
 
+  const lyrics = data?.sort((a: Lyric, b: Lyric) => a.order - b.order) || []
+  const active = () =>
+    lyrics.indexOf(lyrics.find((l) => activeLyricId === l.id) as Lyric)
+
   const setNextSongVerse = () => {
-    const lyrics = data?.sort((a: Lyric, b: Lyric) => a.order - b.order) || []
     let nextSongVerse
     if (activeLyricId === -1 && data) {
       nextSongVerse = lyrics[0]
     } else {
-      nextSongVerse =
-        lyrics[
-          lyrics.indexOf(lyrics.find((l) => activeLyricId === l.id) as Lyric) +
-            1
-        ]
+      nextSongVerse = lyrics[active() + 1]
     }
     if (nextSongVerse) {
       lyricEmit(nextSongVerse.verse)
@@ -34,17 +33,7 @@ const ActiveLyricProvider = ({ children }: ActiveLyricProviderProps) => {
   }
 
   const setPrevSongVerse = () => {
-    const lyrics = data?.sort((a: Lyric, b: Lyric) => a.order - b.order) || []
-    let prevSongVerse
-    if (activeLyricId === -1 && data) {
-      prevSongVerse = lyrics[0]
-    } else {
-      prevSongVerse =
-        lyrics[
-          lyrics.indexOf(lyrics.find((l) => activeLyricId === l.id) as Lyric) -
-            1
-        ]
-    }
+    const prevSongVerse = lyrics[active() - 1]
     if (prevSongVerse) {
       lyricEmit(prevSongVerse.verse)
       setActiveLyricId(prevSongVerse.id)
