8ccb361f05da9565b40e292ccd13205d24506deb
adrikar <orellanakaren30@gmail.com>
2023-09-05 15:37:44 -0400

    corrigiendo app

--- app/admin/styles/create/page.tsx
+++ app/admin/styles/create/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import StylesCreateView from "@modules/Admin/Styles/StylesCreateView"
+import StylesCreateView from "@modules/Admin/Styles/Components/StylesCreateView"
 
 export default function Chroma() {
   return <StylesCreateView />
