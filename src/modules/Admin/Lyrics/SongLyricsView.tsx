e04881a9e4a5c5c46189e3cd50d8d220ec6c28ad
clauclau <75claudiaflores@gmail.com>
2023-09-11 18:40:31 -0400

    Atomizando songLyricsView

--- src/modules/Admin/Lyrics/SongLyricsView.tsx
+++ src/modules/Admin/Lyrics/SongLyricsView.tsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react"
-import { getLyrics } from "src/common/api/songs/lyrics.api"
 import { Lyric } from "@interfaces/lyrics.interface"
 import { useParams, useRouter } from "next/navigation"
 import { handleFormSubmit } from "./Helpers/hundlerSubmit"
+import { fetchLyrics } from "./FetchLyrics"
+import SongLyricsForm from "./SongLyricsForm"
 export default function SongLyricsView() {
   const [song, setSong] = useState("")
   const [loading, setLoading] = useState(false)
@@ -12,32 +13,21 @@ export default function SongLyricsView() {
   const { id } = router
   const idSong = Number(id)
   useEffect(() => {
-    async function fetchLyrics() {
-      try {
-        const lyricResponse = await getLyrics(idSong)
-        const sortedLyrics = lyricResponse.sort(
-          (a: Lyric, b: Lyric) => a.order - b.order
-        )
-        setLyrics(sortedLyrics)
-        if (sortedLyrics.length > 0) {
-          const allVerses = sortedLyrics
-            .map((lyric) => lyric.verse)
-            .join("\n\n")
-          setSong(allVerses)
-        }
-      } catch {
-        // letra vacia
+    async function fetchData() {
+      const lyricsData = await fetchLyrics(idSong)
+      setLyrics(lyricsData)
+      if (lyricsData.length > 0) {
+        const allVerses = lyricsData.map((lyric) => lyric.verse).join("\n\n")
+        setSong(allVerses)
       }
     }
-    fetchLyrics()
+    fetchData()
   }, [idSong])
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     await handleFormSubmit(Number(idSong), song, lyrics, setSong, setLoading)
-
-    // Check if setLoading is now false
     if (!loading) {
-      // Navigate to the desired route
       nextRouter.push(`/admin/songs/${idSong}`)
     }
   }
@@ -46,29 +36,12 @@ export default function SongLyricsView() {
     <div className="max-w-[64rem] mx-auto pt-8">
       <h1 className="mb-4 text-3xl">Editar letra a la canción {idSong}</h1>
       <div>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label className="mb-4" htmlFor="song">
-              Letra
-            </label>
-            <textarea
-              className="input text-ww-normal"
-              name="song"
-              id="song"
-              rows={25}
-              value={song}
-              onChange={(e) => setSong(e.target.value)}
-              required
-            />
-          </div>
-          <div></div>
-          <input
-            className="p-2 bg-ww-green-700 hover:bg-ww-green-800 input"
-            type="submit"
-            value={loading ? "Cargando..." : "Enviar"}
-            disabled={loading}
-          />
-        </form>
+        <SongLyricsForm
+          song={song}
+          loading={loading}
+          handleSubmit={handleSubmit}
+          setSong={setSong}
+        />
       </div>
     </div>
   )
