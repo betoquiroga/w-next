0677c6fe9a16b825384e24072c2e29bb31e49e42
clauclau <75claudiaflores@gmail.com>
2023-09-11 19:30:43 -0400

    Anadiendo un boton a la la vista de letras

--- app/admin/songs/[id]/page.tsx
+++ app/admin/songs/[id]/page.tsx
@@ -3,12 +3,14 @@
 import { Lyric } from "@interfaces/lyrics.interface"
 import { Song } from "@interfaces/song.interface"
 import axios, { HttpStatusCode } from "axios"
+import { useRouter } from "next/navigation"
 import { useEffect, useState } from "react"
 import { WW_API_DOMAIN } from "src/common/constants/domains"
 
 export default function SongId({ params }: SongIdProps) {
   const [song, setSong] = useState<Song | undefined>()
   const [lyrics, setLyrics] = useState<Lyric[] | undefined>()
+  const router = useRouter()
 
   useEffect(() => {
     axios
@@ -44,8 +46,17 @@ export default function SongId({ params }: SongIdProps) {
     )
   }
 
+  const redirectToSongList = () => {
+    router.push("/admin/songs")
+  }
   return (
     <div className="px-24 py-10">
+      <button
+        onClick={redirectToSongList}
+        className="top-0 right-0 m-0 p-2 bg-ww-green-700 hover:bg-ww-green-800"
+      >
+        Ir a la lista de canciones
+      </button>
       <div className="mb-4">
         <h1 className="text-4xl">{song.title}</h1>
         <span className="text-xl text-slate-400">{song.author}</span>
