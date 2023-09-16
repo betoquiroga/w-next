9ccacc4cb43a59dc0b17c8f495788410743d9100
Beto Quiroga <beto.m12@gmail.com>
2023-09-15 23:05:01 -0400

    Open Graph

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,28 @@
-"use client"
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  openGraph: {
+    title: "En Su Palabra - Wiswords",
+    description: "Cancionero digital en vivo",
+    url: "https://esp.wiswords.com",
+    siteName: "Wiswords - ESP",
+    images: [
+      {
+        url: "https://wiswords.com/og.jpg",
+        width: 1280,
+        height: 720,
+      },
+      {
+        url: "https://wiswords.com/og-alt.jpg",
+        width: 1280,
+        height: 720,
+        alt: "En Su Palabra - Wiswords",
+      },
+    ],
+    locale: "es_BO",
+    type: "website",
+  },
+}
 
 import HomeView from "@modules/Home/HomeView"
 
