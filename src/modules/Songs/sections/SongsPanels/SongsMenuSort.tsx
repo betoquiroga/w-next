2386ce653bde8fc4a6d10157eb5c76aca39cc85b
clauclau <75claudiaflores@gmail.com>
2023-09-21 08:56:04 -0400

    Se creo la clase sort.css

--- src/modules/Songs/sections/SongsPanels/SongsMenuSort.tsx
+++ src/modules/Songs/sections/SongsPanels/SongsMenuSort.tsx
@@ -7,25 +7,25 @@ const SongsMenuSort = ({
       <div className="relative">
         <div className="absolute right-0 !important mt-4 p-2 bg-ww-content border border-gray-300 rounded shadow-lg w-40">
           <div
-            className="cursor-pointer hover:bg-ww-green-700 p-2"
+            className="sort-song"
             onClick={() => handleOptionClick("Opción 3")}
           >
             Recientes
           </div>
           <div
-            className="cursor-pointer hover:bg-ww-green-700 p-2"
+            className="sort-song"
             onClick={() => handleOptionClick("Opción 4")}
           >
             Anteriores
           </div>
           <div
-            className="cursor-pointer hover:bg-ww-green-700 p-2"
+            className="sort-song"
             onClick={() => handleOptionClick("Opción 2")}
           >
             Título
           </div>
           <div
-            className="cursor-pointer hover:bg-ww-green-700 p-2"
+            className="sort-song"
             onClick={() => handleOptionClick("Opción 1")}
           >
             Autor
