203ca1644f9f04e5a85ca4cbd90d414fbfda2d82
Beto Quiroga <beto.m12@gmail.com>
2023-09-29 09:55:08 -0400

    quita console.log

--- src/common/helpers/socket/emit.ts
+++ src/common/helpers/socket/emit.ts
@@ -58,7 +58,6 @@ export const verseEmit = (content: string) => {
 }
 
 export const styleEmit = (content: Style) => {
-  console.log(content)
   const image = path.basename(content.image)
   updateScreen(WW_DEFAULT_SCREEN_ID, {
     background: image,
